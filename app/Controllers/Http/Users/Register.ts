c210400a734beb5d388c3b81f2824e4e1156977a
Gabriel <gabrielmesquita0503@gmail.com>
2023-12-19 16:42:51 -0300

    finalizando cadastro

--- app/Controllers/Http/Users/Register.ts
+++ app/Controllers/Http/Users/Register.ts
@@ -1,5 +1,5 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
-import { StoreValidator } from 'App/Validators/User/Register'
+import { StoreValidator, UpdateValidator } from 'App/Validators/User/Register'
 import { User, UserKey } from 'App/Models'
 import { faker } from '@faker-js/faker'
 import Mail from '@ioc:Adonis/Addons/Mail'
@@ -19,20 +19,34 @@ export default class UserResgisterController {
 
     // envio do email
 
-    await Mail.send((message)=>{
+    await Mail.send((message) => {
       message.to(email)
       message.from('contato@teste.com')
       message.subject('Criação de conta')
-      message.htmlView('emails/register', {link})
+      message.htmlView('emails/register', { link })
     })
   }
 
-  public async show({params}: HttpContextContract) {
+  public async show({ params }: HttpContextContract) {
     const userKey = await UserKey.findByOrFail('key', params.key)
     const user = await userKey.related('user').query().firstOrFail()
 
     return user
   }
 
-  public async update({}: HttpContextContract) {}
+  public async update({ request, response }: HttpContextContract) {
+    const { key, name, password } = await request.validate(UpdateValidator)
+    const userKey = await UserKey.findByOrFail('key', key)
+    const user = await userKey.related('user').query().firstOrFail()
+
+    const username = name.split('')[0].toLocaleLowerCase() + new Date().getTime()
+
+    user.merge({ name, password, username })
+
+    await user.save()
+
+    await userKey.delete()
+
+    return response.ok({message: 'ok'})
+  }
 }
